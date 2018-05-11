9b6bfba7bc7f516e25ae22584b96cc9c3a00307b
Franz <franzderocs@gmail.com>
2018-05-11 20:29:21 +0800

    First Commit

---
+++ SpectresSolutions/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+
+namespace SpectresSolutions.Models
+{
+    public class User : IdentityUser 
+    {
+        public string Emal { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ SpectresSolutions/css/Role.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+
+namespace SpectresSolutions.css
+{
+    public class Role : IdentityUser
+    {
+        public Role() : base() { }
+        public Role(string name) : base(name) { }
+    }
+}
